0b16900ec363efb582525f1e17d174a66ae0e3a4
Adam Retter <adam.retter@googlemail.com>
2011-06-05 13:35:49 +0000

    Fix to sql script
    git-svn-id: https://seewhatithink.svn.sourceforge.net/svnroot/seewhatithink/trunk@42 4bd0fe05-8733-449a-9ecc-25b3248bf382

--- applications/Printer-database/create-database.sql
+++ applications/Printer-database/create-database.sql
@@ -1,5 +1,8 @@
-create database xmlss_printer;
+create database xmlss_printer
+GO
 
-CREATE USER 'xmlss_printer'@'localhost' IDENTIFIED BY 'xmlss_printer';
+CREATE USER 'xmlss_printer'@'localhost' IDENTIFIED BY 'xmlss_printer'
+GO
 
-GRANT ALL PRIVILEGES ON xmlss_printer.* TO 'xmlss_printer'@'localhost'
+GRANT ALL ON xmlss_printer.* TO 'xmlss_printer'@'localhost'
+GO
